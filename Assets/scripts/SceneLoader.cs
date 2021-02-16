19918e850a1cfb3a5857a7097e4f223e1c94939d
kshemankarjain <67746254+kshemankarjain@users.noreply.github.com>
2021-02-16 16:06:54 +0530

    new changes

---
+++ Assets/scripts/SceneLoader.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int CurrentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(CurrentSceneIndex + 1);
+    }
+    public void ApplicationQuit()
+    {
+        Application.Quit();
+    }
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene("Start Menu");
+        FindObjectOfType<GameStatus>().ResetGame();
+    }
+    public void LoadLevelScreen()
+    {
+        SceneManager.LoadScene("ChooseLevel");
+    }
+    public void loaddinglevel1()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+    public void loaddingleve2()
+    {
+        SceneManager.LoadScene("Level 2");
+    } public void loaddinglevel3()
+    {
+        SceneManager.LoadScene("Level 3");
+    }
+    public void loaddinglevel4()
+    {
+        SceneManager.LoadScene("Level 4");
+    }
+    public void loaddinglevel5()
+    {
+        SceneManager.LoadScene("Level 5");
+    }
+
+
+}
